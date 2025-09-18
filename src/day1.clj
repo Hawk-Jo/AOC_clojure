532d94c7c365ad72cf2cd59975badbf847fdf03b
Hawk-Jo <johawk5764@gmail.com>
2025-09-18 22:05:25 +0900

    day1_part1

--- src/day1.clj
+++ src/day1.clj
@@ -1,13 +1,10 @@
 (ns day1)
 
 
-(comment
-  (+ 2 2)
-  (->> (range 101)
-       (filter even?)
-       (map #(* % %)))
-  :rcf)
+(require '[clojure.java.io :as io])
 
-(comment
-  (println "hello")
-  :rcf)
+(def nums
+  (with-open [r (io/reader (io/resource "resources/day1_input.txt"))]
+   (doall (map #(Integer/parseInt %)
+               (line-seq r)))))
+(reduce + nums)
